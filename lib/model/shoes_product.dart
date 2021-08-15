bc560336b573c60fd25f64682ca4dfc5dc379a06
Mehrab Bozorgi <mehrab.bozorgi@gmail.com>
2021-08-15 08:40:48 +0430

    part one

---
+++ lib/model/shoes_product.dart
@@ -0,0 +1,15 @@
+class ShoesProduct {
+  final int id;
+  final String title;
+  final String image;
+  final String description;
+  final double price;
+
+  ShoesProduct({
+    required this.id,
+    required this.title,
+    required this.image,
+    required this.description,
+    required this.price,
+  });
+}
